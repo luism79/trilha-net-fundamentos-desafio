4828274b953fc8d53042b8a34b07782c567c7402
luism79 <luis.m7982@hotmail.com>
2026-02-01 10:30:56 -0300

    Validação Placa
      - adicionado validação placa
        # modelo antigo
        # mercosul

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -1,17 +1,18 @@
 using System.Drawing;
+using System.Text.RegularExpressions;
 
 namespace DesafioFundamentos.Models
 {
     public class Estacionamento
     {
-        private decimal precoInicial = 0;
-        private decimal precoPorHora = 0;
-        private List<string> veiculos = new List<string>();
+        private decimal _precoInicial = 0;
+        private decimal _precoPorHora = 0;
+        private List<string> _veiculos = new List<string>();
 
         public Estacionamento(decimal precoInicial, decimal precoPorHora)
         {
-            this.precoInicial = precoInicial;
-            this.precoPorHora = precoPorHora;
+            this._precoInicial = precoInicial;
+            this._precoPorHora = precoPorHora;
         }
 
         public void AdicionarVeiculo()
@@ -21,20 +22,19 @@ namespace DesafioFundamentos.Models
             
             string placa = Console.ReadLine();
 
-            if (!string.IsNullOrEmpty(placa))
+            if (!ValidarPlaca(placa))
             {
-                bool existe = veiculos.Any(x => x.ToUpper() == placa.ToUpper());
-                if (existe)
-                {
-                    Console.WriteLine("Veículo já está estacionado.");
-                    return;
-                }
-                veiculos.Add(placa.ToUpper());
+                Console.WriteLine("Placa inválida. Utilize o formato ABC-1234 ou ABC1D23.");
+                return;
             }
-            else
+            
+            bool existe = _veiculos.Any(x => x.ToUpper() == placa.ToUpper());
+            if (existe)
             {
-                Console.WriteLine("Placa não pode ser em branco.");
+                Console.WriteLine("Veículo já está estacionado.");
+                return;
             }
+            _veiculos.Add(FormatarPlaca(placa));
         }
 
         public void RemoverVeiculo()
@@ -42,10 +42,10 @@ namespace DesafioFundamentos.Models
             Console.Write("\nDigite a placa do veículo para remover: ");
 
             // Pedir para o usuário digitar a placa e armazenar na variável placa
-            string placa = Console.ReadLine();
+            string placa = FormatarPlaca(Console.ReadLine());
 
             // Verifica se o veículo existe
-            if (veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
+            if (_veiculos.Any(x => x.ToUpper() == placa.ToUpper()))
             {
                 Console.Write("\nDigite a quantidade de horas que o veículo permaneceu estacionado: ");
                 string horasInput = Console.ReadLine();
@@ -57,20 +57,20 @@ namespace DesafioFundamentos.Models
                     Console.WriteLine("Quantidade de horas inválida.");
                     return;
                 }
-                decimal valorTotal = precoInicial + precoPorHora * horas; 
+                decimal valorTotal = _precoInicial + _precoPorHora * horas; 
 
                 // TODO: Remover a placa digitada da lista de veículos
                 
-                veiculos.Remove(placa.ToUpper());
+                _veiculos.Remove(placa.ToUpper());
 
                 Console.WriteLine("");
                 Console.WriteLine("".PadLeft(37, '*'));
                 Console.WriteLine("*      Listagem de finalização      *");
                 Console.WriteLine("".PadLeft(37, '*'));
-                Console.WriteLine($"   Veículo.......: {placa.ToUpper()}");
+                Console.WriteLine($"   Veículo.......: {placa.ToUpper().PadLeft(8, ' ')}");
                 Console.WriteLine($"   Horas.........: {horas, 8}");
-                Console.WriteLine($"   Valor.........: {precoInicial, 8:N2}");
-                Console.WriteLine($"   Valor / Hora..: {precoPorHora, 8:N2}");
+                Console.WriteLine($"   Valor.........: {_precoInicial, 8:N2}");
+                Console.WriteLine($"   Valor / Hora..: {_precoPorHora, 8:N2}");
                 Console.WriteLine("".PadLeft(37, '-'));
                 Console.WriteLine($"   Valor Total...: {valorTotal, 8:N2}");
                 Console.WriteLine("".PadLeft(37, '-'));
@@ -85,26 +85,52 @@ namespace DesafioFundamentos.Models
         {
             // Verifica se há veículos no estacionamento
             Console.WriteLine("");
-            if (veiculos.Any())
+            if (_veiculos.Any())
             {
                 Console.WriteLine("".PadLeft(37, '*'));
                 Console.WriteLine("*       Listagem de Veículos        *");
                 Console.WriteLine("".PadLeft(37, '*'));
 
                 int cont = 1;
-                foreach (var veiculo in veiculos)
+                foreach (var veiculo in _veiculos)
                 {
                     Console.WriteLine($"  {cont.ToString("00")} - {veiculo}");
                     cont++;
                 }
                 Console.WriteLine("".PadLeft(37, '-'));
-                Console.WriteLine($"Total de veículos estacionados: {veiculos.Count.ToString("00")}");
+                Console.WriteLine($"Total de veículos estacionados: {_veiculos.Count.ToString("00")}");
                 Console.WriteLine("".PadLeft(37, '-'));
             }
             else
             {
                 Console.WriteLine("Não há veículos estacionados.");
             }
+        }
+
+        private bool ValidarPlaca(string placa)
+        {
+            // Formato de placa: ABC-1234 ou ABC1D23
+            var regex = new System.Text.RegularExpressions.Regex("^[A-Z]{3}-?[0-9][A-Z0-9][0-9]{2}$");
+            return regex.IsMatch(placa.ToUpper());
+        }
+
+        private bool ValidarPlacaMercosul(string placa)
+        {
+            // Formato de placa Mercosul: ABC1D23
+            var regex = new System.Text.RegularExpressions.Regex("^[A-Z]{3}\\d[A-Z]\\d{2}$");
+            return regex.IsMatch(placa.ToUpper());
+        }
+
+        private string FormatarPlaca(string placa)
+        {
+            if (ValidarPlacaMercosul(placa))
+            {
+                return placa.ToUpper();
+            }
+            
+            // Formatar placa para o padrão ABC-1234
+            placa = placa.ToUpper().Replace("-", "");
+            return placa.Substring(0, 3) + "-" + placa.Substring(3, 4);
         }
     }
 }
